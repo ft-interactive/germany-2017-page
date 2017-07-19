1ff6b16be4ceef006ad0bab2105379bf33039fff
ajwl <anna.leach@ft.com>
2017-07-19 12:11:12 +0100

    add text to german ig page

--- config/article.js
+++ config/article.js
@@ -11,7 +11,7 @@ export default () => ({ // eslint-disable-line
   //       new Date('2016-05-17T17:11:22Z')
   publishedDate: new Date(),
 
-  headline: 'German Federal Election Polltracker',
+  headline: 'German federal election 2017 polltracker',
 
   // summary === standfirst (Summary is what the content API calls it)
   summary: 'Angela Merkel is vying for a fourth term as Chancellor ' +
@@ -33,7 +33,7 @@ export default () => ({ // eslint-disable-line
     credit: '',
 
     // You can provide a UUID to an image and it was populate everything else
-    uuid: 'c4bf0be4-7c15-11e4-a7b8-00144feabdc0',
+    uuid: '',
 
     // You can also provide a URL
     // url: 'https://image.webservices.ft.com/v1/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2Fc4bf0be4-7c15-11e4-a7b8-00144feabdc0?source=ig&fit=scale-down&width=700',
