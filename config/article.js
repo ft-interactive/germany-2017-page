97a8909d4571d000e1f2418c22d4a26d2e3e22a3
Martin Stabe <martinstabe@gmail.com>
2017-08-14 16:08:00 +0100

    Typo fix

--- config/article.js
+++ config/article.js
@@ -11,7 +11,7 @@ export default () => ({ // eslint-disable-line
   //       new Date('2016-05-17T17:11:22Z')
   publishedDate: new Date(),
 
-  headline: 'German election 2017 polltracker',
+  headline: 'German election 2017 poll tracker',
 
   // summary === standfirst (Summary is what the content API calls it)
   summary: 'Angela Merkel is vying for a fourth term as chancellor when Germany elects a new Bundestag on September 24',
